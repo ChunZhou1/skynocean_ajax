f23000d410ac84bd53800baf427bd7d3762496b6
Jack.study <njzhch@163.com>
2021-03-07 19:13:18 -0500

    ajax complete

--- src/component/product_component.js
+++ src/component/product_component.js
@@ -11,228 +11,7 @@ import async from "async";
 import "../bootstrap/css/bootstrap.min.css";
 import css from "./product_component.css";
 
-import cent_1 from "../../images/cent_1.png";
-import cent_2 from "../../images/cent_2.png";
-import cent_3 from "../../images/cent_3.png";
-import cent_4 from "../../images/cent_4.png";
-import cent_5 from "../../images/cent_5.png";
-import cent_6 from "../../images/cent_6.png";
-import cent_7 from "../../images/cent_7.png";
-import cent_8 from "../../images/cent_8.png";
-import cent_9 from "../../images/cent_9.png";
-import cent_10 from "../../images/cent_10.png";
-import cent_11 from "../../images/cent_11.png";
-import cent_12 from "../../images/cent_12.png";
-import cent_13 from "../../images/cent_13.png";
-import cent_14 from "../../images/cent_14.png";
-import cent_15 from "../../images/cent_15.png";
-import cent_16 from "../../images/cent_16.png";
-import cent_17 from "../../images/cent_17.png";
-import cent_18 from "../../images/cent_18.png";
-import cent_19 from "../../images/cent_19.png";
-import cent_20 from "../../images/cent_20.png";
-import cent_21 from "../../images/cent_21.png";
-import cent_22 from "../../images/cent_22.png";
-import cent_23 from "../../images/cent_23.png";
-import cent_24 from "../../images/cent_24.png";
-import cent_25 from "../../images/cent_25.png";
-import cent_26 from "../../images/cent_26.png";
-
-import cat_1 from "../../images/cat_1.png";
-import cat_2 from "../../images/cat_2.png";
-import cat_3 from "../../images/cat_3.png";
-import cat_4 from "../../images/cat_4.png";
-
 import service from "../../images/service.png";
-
-/*const product_catalog = [
-  {
-    id: 1,
-    catalog_name: "Smart Home",
-    catalog_pic: cat_1
-  },
-
-  {
-    id: 2,
-    catalog_name: "Smart Manufacturing",
-    catalog_pic: cat_2
-  },
-
-  {
-    id: 3,
-    catalog_name: "Smart health",
-    catalog_pic: cat_3
-  },
-
-  {
-    id: 4,
-    catalog_name: "Smart Farming - Agriculture",
-    catalog_pic: cat_4
-  }
-];
-
-const product = [
-  {
-    id: 1,
-    content: "Home with Home Hub and Home Mini on table",
-    pic_content: cent_1,
-    catalog: "Smart Home"
-  },
-
-  {
-    id: 2,
-    content: "HomeKit_Hero_CAN@2x",
-    pic_content: cent_2,
-    catalog: "Smart Home"
-  },
-  {
-    id: 3,
-    content: "IOT-2",
-    pic_content: cent_3,
-    catalog: "Smart Home"
-  },
-  {
-    id: 4,
-    content: "IOT-3",
-    pic_content: cent_4,
-    catalog: "Smart Home"
-  },
-
-  {
-    id: 5,
-    content: "google nest smart home devices",
-    pic_content: cent_5,
-    catalog: "Smart Home"
-  },
-  {
-    id: 6,
-    content: "smart-home",
-    pic_content: cent_6,
-    catalog: "Smart Home"
-  },
-  {
-    id: 7,
-    content: "smarthome-pixabay-resized",
-    pic_content: cent_7,
-    catalog: "Smart Home"
-  },
-
-  {
-    id: 8,
-    content: "Photo-for-julies Google-Home-story",
-    pic_content: cent_8,
-    catalog: "Smart Home"
-  },
-  {
-    id: 9,
-    content: "IOT Smart Home",
-    pic_content: cent_9,
-    catalog: "Smart Home"
-  },
-  {
-    id: 10,
-    content: "IIOT SmartFactory",
-    pic_content: cent_10,
-    catalog: "Smart Manufacturing"
-  },
-
-  {
-    id: 11,
-    content: "Industry 4 point 0",
-    pic_content: cent_11,
-    catalog: "Smart Manufacturing"
-  },
-  {
-    id: 12,
-    content: "IoT",
-    pic_content: cent_12,
-    catalog: "Smart Manufacturing"
-  },
-  {
-    id: 13,
-    content: "IOT_4_Compressed",
-    pic_content: cent_13,
-    catalog: "Smart Manufacturing"
-  },
-  {
-    id: 14,
-    content: "Iot-In-Manufacturing",
-    pic_content: cent_14,
-    catalog: "Smart Manufacturing"
-  },
-  {
-    id: 15,
-    content: "iot-in-manufacturing-main",
-    pic_content: cent_15,
-    catalog: "Smart Manufacturing"
-  },
-  {
-    id: 16,
-    content: "Iot-for-Manufacturing",
-    pic_content: cent_16,
-    catalog: "Smart Manufacturing"
-  },
-  {
-    id: 17,
-    content: "Smart Health",
-    pic_content: cent_17,
-    catalog: "Smart health"
-  },
-  {
-    id: 18,
-    content: "Zwift training",
-    pic_content: cent_18,
-    catalog: "Smart health"
-  },
-  {
-    id: 19,
-    content: "Zwift running",
-    pic_content: cent_19,
-    catalog: "Smart health"
-  },
-  {
-    id: 20,
-    content: "Zwift running",
-    pic_content: cent_20,
-    catalog: "Smart health"
-  },
-  {
-    id: 21,
-    content: "Zwift running",
-    pic_content: cent_21,
-    catalog: "Smart health"
-  },
-  {
-    id: 22,
-    content: "Agriculture-iot",
-    pic_content: cent_22,
-    catalog: "Smart Farming - Agriculture"
-  },
-  {
-    id: 23,
-    content: "App-of-iot-in-Agriculture",
-    pic_content: cent_23,
-    catalog: "Smart Farming - Agriculture"
-  },
-  {
-    id: 24,
-    content: "climate condition",
-    pic_content: cent_24,
-    catalog: "Smart Farming - Agriculture"
-  },
-  {
-    id: 25,
-    content: "b7auuaow-smart-farming-iot",
-    pic_content: cent_25,
-    catalog: "Smart Farming - Agriculture"
-  },
-  {
-    id: 26,
-    content: "Smart-Greenhouse",
-    pic_content: cent_26,
-    catalog: "Smart Farming - Agriculture"
-  }
-];*/
 
 function Product_main_container(props) {
   return (
@@ -325,17 +104,33 @@ function Product_text(props) {
 
 //The function below used to ajax req
 
-async function getPictures(length) {
+async function getPictures_product(input_obj) {
   let reqList = [];
 
   let resList = [];
 
-  console.log(length);
+  for (var i = 0; i < input_obj.data.length; i++) {
+    let req_url = "/get/picture/" + input_obj.data[i].pic_content + ".png";
 
-  for (let i = 0; i < length; i++) {
-    let req_url = "/get/picture/" + "cent_" + (i + 1) + ".png";
+    let req = axios.get(req_url, { responseType: "blob" });
 
-    /*console.log(req_url);*/
+    reqList.push(req);
+    resList.push();
+  }
+
+  return axios.all(reqList).then(
+    axios.spread(function(...resList) {
+      return resList;
+    })
+  );
+}
+
+async function getPictures_catalog(input_obj) {
+  let reqList = [];
+  let resList = [];
+
+  for (var i = 0; i < input_obj.data.length; i++) {
+    let req_url = "/get/picture/" + input_obj.data[i].catalog_pic + ".png";
 
     let req = axios.get(req_url, { responseType: "blob" });
 
@@ -365,19 +160,42 @@ function process_product(product, input_picture) {
   return product_t;
 }
 
+function process_product_catalog(product_catalog, input_picture) {
+  var blob, objectURL, product_catalog_t;
+
+  product_catalog_t = product_catalog.data;
+
+  for (var i = 0; i < product_catalog_t.length; i++) {
+    blob = new Blob([input_picture[i].data]);
+    objectURL = URL.createObjectURL(blob);
+
+    product_catalog_t[i].catalog_pic = objectURL;
+  }
+
+  return product_catalog_t;
+}
+
 async function req_product(obj) {
-  var result_p, result_product, result_catalog, result_product_e;
+  var result_p,
+    result_product,
+    result_catalog,
+    result_product_e,
+    result_catalog_e;
   result_product = await axios.get("/get/json/product");
 
   result_catalog = await axios.get("/get/json/product_catalog");
 
-  result_p = await getPictures(result_product.data.length);
+  result_p = await getPictures_product(result_product);
 
   result_product_e = process_product(result_product, result_p);
 
+  result_p = await getPictures_catalog(result_catalog);
+
+  result_catalog_e = process_product_catalog(result_catalog, result_p);
+
   obj.setState({
     product: result_product_e,
-    product_catalog: result_catalog.data
+    product_catalog: result_catalog_e
   });
 }
 
